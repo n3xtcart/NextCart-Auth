35fd0a30f8d459b60b2c45bde3331bf099f5ddd6
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-04-14 14:49:25 +0200

    Aggiunto Dao

---
+++ src/main/java/it/nextre/corsojava/dao/Dao.java
@@ -0,0 +1,28 @@
+package it.nextre.corsojava.dao;
+
+import it.nextre.corsojava.entity.Entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Dao<T extends Entity> {
+
+    private final Map<Long, T> database;
+    protected Long idGenerator;
+
+    protected Dao() {
+        this.idGenerator = 1L;
+        this.database = new HashMap<>();
+    }
+
+    public void add(T item) {
+        item.setId(idGenerator++);
+        database.put(item.getId(), item);
+    }
+
+    public abstract void update(Long id, T item);
+
+    public void delete(Long id) {
+        database.remove(id);
+    }
+}
