f0aafeda98ae94317c85476497891609d944e729
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-04-14 15:10:13 +0200

    Implementato dao admin

--- src/main/java/it/nextre/corsojava/dao/AdminDAO.java
+++ src/main/java/it/nextre/corsojava/dao/AdminDAO.java
@@ -6,11 +6,12 @@ public class AdminDAO extends Dao<Admin> {
 
     @Override
     public void update(Long id, Admin item) {
-        Admin admin = database.get(id);
-        if (item.getNome() != null) admin.setNome(item.getNome());
-        if (item.getCognome() != null) admin.setCognome(item.getCognome());
-        if (item.getEmail() != null) admin.setEmail(item.getEmail());
-        if (item.getRole() != null) admin.setRole(item.getRole());
-        if (item.getPassword() != null) admin.setPassword(item.getPassword());
+        Admin toModify = database.get(id);
+        if (item.getNome() != null) toModify.setNome(item.getNome());
+        if (item.getCognome() != null) toModify.setCognome(item.getCognome());
+        if (item.getEmail() != null) toModify.setEmail(item.getEmail());
+        if (item.getRole() != null) toModify.setRole(item.getRole());
+        if (item.getPassword() != null) toModify.setPassword(item.getPassword());
+        database.put(toModify.getId(), toModify);
     }
 }
--- src/main/java/it/nextre/corsojava/dao/UserDAO.java
+++ src/main/java/it/nextre/corsojava/dao/UserDAO.java
@@ -3,28 +3,16 @@ package it.nextre.corsojava.dao;
 
 import it.nextre.corsojava.entity.User;
 
-public class UserDAO  extends Dao<User> {
+public class UserDAO extends Dao<User> {
 
-	@Override
-	public void update(Long id, User item) {
-		User user = database.get(id);
-		user.setNome(item.getNome());
-		user.setEmail(item.getEmail());
-		user.setPassword(item.getPassword());
-		user.setCognome(item.getCognome());	
-		user.setRole(item.getRole());
-		
-		
-	}
-
-
-
-	
-	
-
-		
-		
-		
-	
-
+    @Override
+    public void update(Long id, User item) {
+        User user = database.get(id);
+        user.setNome(item.getNome());
+        user.setEmail(item.getEmail());
+        user.setPassword(item.getPassword());
+        user.setCognome(item.getCognome());
+        user.setRole(item.getRole());
+        database.put(user.getId(), user);
+    }
 }
