89d841e7c9db1ca44522ea001160b32a6d1b5047
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-04-14 15:06:37 +0200

    Implementato dao admin

--- src/main/java/it/nextre/corsojava/dao/AdminDAO.java
+++ src/main/java/it/nextre/corsojava/dao/AdminDAO.java
@@ -6,6 +6,11 @@ public class AdminDAO extends Dao<Admin> {
 
     @Override
     public void update(Long id, Admin item) {
-
+        Admin admin = database.get(id);
+        if (item.getNome() != null) admin.setNome(item.getNome());
+        if (item.getCognome() != null) admin.setCognome(item.getCognome());
+        if (item.getEmail() != null) admin.setEmail(item.getEmail());
+        if (item.getRole() != null) admin.setRole(item.getRole());
+        if (item.getPassword() != null) admin.setPassword(item.getPassword());
     }
 }
