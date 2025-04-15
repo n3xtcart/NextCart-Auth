10cb750dab5c0f7c9ed7e5c013365b27fcc6636d
salvatore <100683918+SalvatoreScarfone1999@users.noreply.github.com>
2025-04-15 14:31:37 +0200

    userService

--- src/main/java/it/nextre/corsojava/service/UserService.java
+++ src/main/java/it/nextre/corsojava/service/UserService.java
@@ -1,5 +1,19 @@
 package it.nextre.corsojava.service;
 
+import it.nextre.corsojava.dao.GroupDAO;
+import it.nextre.corsojava.dao.RoleDAO;
+import it.nextre.corsojava.dao.TokenAdminDAO;
+import it.nextre.corsojava.dao.TokenUserDAO;
+import it.nextre.corsojava.dao.UserDAO;
+
 public class UserService {
+	private UserDAO userDAO;
+	private TokenAdminDAO tokenService;
+	private TokenUserDAO tokenUserDAO;
+	private GroupDAO groupDAO;
+	private RoleDAO roleDAO;
+
+
+	
 
 }
